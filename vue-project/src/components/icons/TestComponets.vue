6db1499093bd949b3120eb9872121fe389928b38
BrianZ26 <brian.zhao026@gmail.com>
2023-03-08 13:58:58 -0500

    A

---
+++ vue-project/src/components/icons/TestComponets.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.text = e.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <input :value="text" @input="onInput" placeholder="Type here" />
+  <p>{{ text }}</p>
+  <h1>hello</h1>
+</template>

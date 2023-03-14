7c5200e39bd8d6847ffb9ff328ad537ddc48429e
BrianZ26 <brian.zhao026@gmail.com>
2023-03-14 14:00:45 -0400

    A

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,44 @@
-<script setup></script>
+<script setup>
+import MainPage from './components/MainPage.vue'
+</script>
 
 <template>
-  <h1>Hello</h1>
+  <header>
+    <div class="wrapper">
+      <MainPage msg="This is my page!" />
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -16,11 +16,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import('../components/icons/TestComponets.vue')
     }
   ]
 })

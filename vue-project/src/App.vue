a0b7a27d88f1d06a92c35979109cfd98059dc834
BrianZ26 <brian.zhao026@gmail.com>
2023-03-20 14:00:31 -0400

    A

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { Ref } from 'vue';
-const Ref toppings = [
-  { name: 'Pepperoni', img: 'https://trottospizza.com/wp-content/uploads/2017/12/pepperoni.png' },
+const toppings = [
+  {
+    name: 'Pepperoni',
+    img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/pepperoni.png'
+  },
   {
     name: 'Sausage',
     img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/beef.png'
@@ -10,19 +12,39 @@ const Ref toppings = [
     name: 'Mushrooms',
     img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/mushrooms.png'
   },
-  { name: 'Bacon' },
-  { name: 'Onions' },
-  { name: 'Peppers' },
-  { name: 'Chicken' }
-])
-console.log(toppings)
+  {
+    name: 'Chicken',
+    img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/chicken.png'
+  },
+  {
+    name: 'Bacon',
+    img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/bacon.png'
+  },
+  {
+    name: 'Cheese',
+    img: 'https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/cheese.png'
+  }
+]
 </script>
 
 <template>
-  <div v-for="topping in toppings">
-    <h1>{{ topping.name }}</h1>
-    <h2>{{ topping.img }}</h2>
+  <div v-for="topping in toppings" class="select">
+    <h1 class="selector-text">{{ topping.name }}</h1>
+    <img :src="topping.img" />
   </div>
 </template>
-
-<style scoped></style>
+<!-- https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/tomato-sauce.png -->
+<!-- https://212newyorkpizza.com/wp-content/uploads/2019/01/pizza-crust.png -->
+<style scoped>
+.selector-text {
+  text-align: center;
+}
+.select {
+  align-content: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  border: 5px solid rgb(0, 0, 0);
+  padding: 20px;
+  margin: 20px;
+}
+</style>

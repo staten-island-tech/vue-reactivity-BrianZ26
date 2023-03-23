fa5a321dda9527132f64314b7cadaaaeeb7f8deb
BrianZ26 <brian.zhao026@gmail.com>
2023-03-23 14:01:12 -0400

    A

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,29 +1,18 @@
 <script setup>
 import toppings from '../src/components/ToppingSelector.vue'
+import create from '../src/components/PizzaMain.vue'
 </script>
 
 <template>
   <div class="website">
     <div class="page-header">
       <h1>Test</h1>
-      <div class="create">
-        <img
-          src="https://212newyorkpizza.com/wp-content/uploads/2019/01/pizza-crust.png"
-          class="main"
-        />
-        <img
-          src="https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/tomato-sauce.png"
-          class="main"
-        />
-        <img
-          src="https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/cheese.png"
-          class="main"
-        />
-        <img
-          src="https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/pepperoni.png"
-          class="main"
-        />
-      </div>
+    </div>
+    <div class="display">
+      <toppings></toppings>
+    </div>
+    <div>
+      <create></create>
     </div>
   </div>
 </template>
@@ -39,12 +28,5 @@ h1 {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-}
-.main {
-  position: absolute;
-  top: 25%;
-  width: 100%;
-  width: 500px;
-  align-content: center;
 }
 </style>
---
+++ vue-project/src/components/PizzaMain.vue
@@ -0,0 +1,28 @@
+<script></script>
+<template>
+  <div class="create">
+    <img
+      src="https://212newyorkpizza.com/wp-content/uploads/2019/01/pizza-crust.png"
+      class="main"
+      draggable="false"
+    />
+    <img
+      src="https://212newyorkpizza.com/wp-content/plugins/pizzatime/images/tomato-sauce.png"
+      class="main"
+      draggable="false"
+    />
+  </div>
+</template>
+<style scoped>
+.main {
+  position: absolute;
+  align-content: center;
+  left: 100%;
+}
+.create {
+  width: 25%;
+}
+h1 {
+  align-self: center;
+}
+</style>
